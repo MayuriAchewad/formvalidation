cefebfa25140d98bb0da5ba96e1b1fc3bce3291c
MayuriAchewad <mayuriachewad@gmail.com>
2024-01-02 19:14:25 +0530

    all set

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import "./App.css"
 
 function App() {
   return (
-    <Card sx={{width:{xs:300,sm:400,md:600, lg:700, xl:800}, margin:"0px auto", marginTop:2,bgcolor:"lightgrey",boxShadow:"0px 0px 10px 10px black"}}>
+    <Card className='app' sx={{width:{xs:300,sm:400,md:600, lg:700, xl:800}, margin:"0px auto", marginTop:2,bgcolor:"lightgrey",boxShadow:"0px 0px 10px 10px black"}}>
       <CardContent>
         
         <Landing/>
